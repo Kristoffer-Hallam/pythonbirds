e5ffafdc52248dab52be7660d6a084ddf34e790c
Kristoffer-Hallam <kristoffer.hallam@gmail.com>
2020-08-22 22:50:12 -0300

    Criados atrobutos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,6 +1,12 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
         return 'Olá'
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Amanda')
     print(p.cumprimentar())
+    p.nome = 'Kristoffer'
+    print(p.nome)
+    print(p.idade)
