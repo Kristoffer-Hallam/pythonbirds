cbc04ebe2deb27b1f1387583e8dca35a82e3ff86
Kristoffer-Hallam <kristoffer.hallam@gmail.com>
2020-08-29 22:50:49 -0300

    Implementação do carro

--- oo/carro.py
+++ oo/carro.py
@@ -94,6 +94,29 @@ Ela oferece os seguintes atributos:
     'O'
 """
 
+class Carro:
+    def __init__(self, motor, direcao):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
 NORTE = 'N'
 SUL = 'S'
 LESTE = 'L'
@@ -127,5 +150,5 @@ class Motor:
         self.velocidade -= 2
         self.velocidade = max(0, self.velocidade)
 
-if __name__ == '__main__':
-    carro = Carro
+# if __name__ == '__main__':
+#     carro = Carro
